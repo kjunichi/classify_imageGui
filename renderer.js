2ce061559a707bf59dad4b7c3a53b1d26424b0d5
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2017-02-02 19:43:26 +0900

    Fix linux

--- renderer.js
+++ renderer.js
@@ -5,26 +5,38 @@
 // In the renderer process.
 const electron = require('electron');
 const fs = require('fs');
-const which =require('which');
+const which = require('which');
 const execSync = require('child_process').execSync;
 const exec = require('child_process').exec;
 
 function getPythonPath() {
-    if(process.platform === 'win32') {
-        if(fs.existsSync(`${process.env.USERPROFILE}\\Anaconda3\\python.exe`)) {
-            return `${process.env.USERPROFILE}\\Anaconda3\\python.exe`.replace(/\\/g,"/");
+    if (process.platform === 'win32') {
+        if (fs.existsSync(`${process.env.USERPROFILE}\\Anaconda3\\python.exe`)) {
+            return `${process.env.USERPROFILE}\\Anaconda3\\python.exe`.replace(/\\/g, "/");
         }
     } else {
         // macOS,Linux
+        let pythonBin;
+        try {
+            pythonBin = which.sync('python3');
+        } catch (e) {
+            try {
+                pythonBin = which.sync('python');
+            } catch (e2) {
+
+            }
+        }
+        return pythonBin;
     }
 }
+
 function getTfHome(pythonBin) {
     const buf = execSync(`${pythonBin} -c"import site;print(site.getsitepackages())"`);
-    const libs = buf.toString().replace(/\\\\/g,"/").replace(/'/g,"").replace(/\[/,"").replace(/\]/,"").replace(/\n/,"").split(",");
+    const libs = buf.toString().replace(/\\\\/g, "/").replace(/'/g, "").replace(/\[/, "").replace(/\]/, "").replace(/\n/, "").split(",");
 
-    for(let i =0; i<libs.length; i++) {
+    for (let i = 0; i < libs.length; i++) {
         console.log(libs[i]);
-        if(fs.existsSync(`${libs[i].trim()}/tensorflow`)) {
+        if (fs.existsSync(`${libs[i].trim()}/tensorflow`)) {
             return libs[i];
         }
     }
@@ -33,40 +45,42 @@ function getTfHome(pythonBin) {
 
 const pythonBin = getPythonPath();
 const tfHome = getTfHome(pythonBin);
-    
+
 const con = document.getElementById("con");
 
 con.innerHTML = "";
 
 const image = new Image();
 
-image.onload = function () {
+image.onload = function() {
     console.log(`w,h = ${image.width},${image.height}`);
     const canvas = document.getElementById("world");
     const ctx = canvas.getContext("2d");
     //canvasのリサイズ
     canvas.width = 512;
     canvas.height = 512;
-    if(image.width>image.height) {
-        canvas.height = canvas.width*(image.height/image.width);
+    if (image.width > image.height) {
+        canvas.height = canvas.width * (image.height / image.width);
     } else {
-        canvas.width = canvas.height*(image.width/image.height);
+        canvas.width = canvas.height * (image.width / image.height);
     }
 
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-    
-    const d64str = canvas.toDataURL("image/jpeg").replace(/^data:image\/jpeg;base64,/,"");
-    fs.writeFileSync(`${__dirname}\\dropImage.jpg`, d64str,{encoding: 'base64'});
-    const imagePath = `${__dirname}\\dropImage.jpg`.replace(/\\/g,"/");
-    
+
+    const d64str = canvas.toDataURL("image/jpeg").replace(/^data:image\/jpeg;base64,/, "");
+    const imagePath = `${__dirname}/dropImage.jpg`.replace(/\\\\/g, "/");
+    fs.writeFileSync(imagePath, d64str, {
+        encoding: 'base64'
+    });
+
     exec(`${pythonBin} ${tfHome}/tensorflow/models/image/imagenet/classify_image.py --image_file ${imagePath}`, (err, stdout, stderr) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(stdout);
-    
-    con.innerText = stdout;
-});
+        if (err) {
+            console.log(err);
+        }
+        console.log(stdout);
+
+        con.innerText = stdout;
+    });
 
 };
 
@@ -84,7 +98,7 @@ dropArea.addEventListener('dragleave', (e) => {
 dropArea.addEventListener('drop', (e) => {
     e.preventDefault();
 
-    
+
     var file = e.dataTransfer.files[0];
     if (!file.type.match(/image\/(jpeg|png|gif|svg)/)[1]) {
         // 指定したファイル以外の場合、処理を続行しない。
@@ -93,7 +107,7 @@ dropArea.addEventListener('drop', (e) => {
     }
 
     var reader = new FileReader();
-    reader.onload = function (e) {
+    reader.onload = function(e) {
         con.innerText = 'Reader onload start';
         const fileContent = reader.result;
         image.src = fileContent;
