7a16317eef18d8a6a1e214710272f78ae8cd9c91
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2017-02-05 13:19:12 +0900

    Consider exif orientation

--- renderer.js
+++ renderer.js
@@ -43,49 +43,83 @@ function getTfHome(pythonBin) {
     return "";
 }
 
+function getImageBlob() {
+
+}
+
+function getExif(buffer) {
+    console.log(buffer.length)
+    const oFile = new BinaryFile(buffer);
+    const iLength = oFile.getLength();
+    const iOffset = 2;
+    const oEXIF = EXIF.readFromBinaryFile(oFile);
+
+    EXIF.bDebug = true;
+    return oEXIF;
+}
+
+function getCorrectOrientationImage(img, orientation, w, h) {
+    console.log(`${img},${orientation},${w},${h}`);
+    const c2 = document.createElement("canvas");
+    c2.width = img.imgwidth;
+    c2.height = img.height;
+    let rotD = 0;
+    console.log(`orientation = ${orientation}`)
+    if (orientation == 1) {
+        c2.width = img.width;
+        c2.height = img.height;
+        rotD = 0;
+    } else if (orientation == 6) {
+        c2.width = img.height;
+        c2.height = img.width;
+        rotD = 90;
+    } else if (orientation == 8) {
+        c2.width = img.height;
+        c2.height = img.width;
+        rotD = 270;
+    } else if (orientation == 3) {
+        c2.width = img.height;
+        c2.height = img.width;
+        rotD = 180;
+    }
+    const c2ctx = c2.getContext("2d");
+    c2ctx.save();
+
+    // 平行移動
+    c2ctx.translate(w / 2, h / 2);
+
+    // 回転
+    const rot = rotD / 180 * Math.PI;
+    c2ctx.rotate(rot);
+
+    // 平行移動
+    c2ctx.translate(-w / 2, -h / 2);
+    c2ctx.drawImage(img, 0, 0, w, h);
+
+    c2ctx.restore();
+    return c2ctx.getImageData(0, 0, w, h);
+}
+
+const sendBtn = document.getElementById("sendBtn");
+sendBtn.addEventListener('click', () => {
+    const url = "";
+    fetch(url, {
+        method: 'POST',
+        body: getImageBlob()
+    }).then((res) => {
+        return res.json();
+    }).then((json) => {
+        console.dir(json);
+    });
+}, false);
 const pythonBin = getPythonPath();
 const tfHome = getTfHome(pythonBin);
-
 const con = document.getElementById("con");
 
 con.innerHTML = "";
 
-const image = new Image();
-
-image.onload = function() {
-    console.log(`w,h = ${image.width},${image.height}`);
-    const canvas = document.getElementById("world");
-    const ctx = canvas.getContext("2d");
-    //canvasのリサイズ
-    canvas.width = 512;
-    canvas.height = 512;
-    if (image.width > image.height) {
-        canvas.height = canvas.width * (image.height / image.width);
-    } else {
-        canvas.width = canvas.height * (image.width / image.height);
-    }
-
-    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-
-    const d64str = canvas.toDataURL("image/jpeg").replace(/^data:image\/jpeg;base64,/, "");
-    const imagePath = `${__dirname}/dropImage.jpg`.replace(/\\\\/g, "/");
-    fs.writeFileSync(imagePath, d64str, {
-        encoding: 'base64'
-    });
-
-    exec(`${pythonBin} ${tfHome}/tensorflow/models/image/imagenet/classify_image.py --image_file ${imagePath}`, (err, stdout, stderr) => {
-        if (err) {
-            console.log(err);
-        }
-        console.log(stdout);
-
-        con.innerText = stdout;
-    });
-
-};
-
 //ドロップされるエリアの取得
-var dropArea = document.getElementById('dropImg');
+const dropArea = document.getElementById('dropImg');
 
 dropArea.addEventListener('dragover', (e) => {
     e.preventDefault();
@@ -98,33 +132,55 @@ dropArea.addEventListener('dragleave', (e) => {
 dropArea.addEventListener('drop', (e) => {
     e.preventDefault();
 
-
-    var file = e.dataTransfer.files[0];
-    if (!file.type.match(/image\/(jpeg|png|gif|svg)/)[1]) {
+    const file = e.dataTransfer.files[0];
+    if (!file.type.match(/image\/jpeg/)) {
         // 指定したファイル以外の場合、処理を続行しない。
         e.stopPropagation();
         return false;
     }
 
-    var reader = new FileReader();
-    reader.onload = function(e) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
         con.innerText = 'Reader onload start';
-        const fileContent = reader.result;
-        image.src = fileContent;
+
+        const buffer = new Uint8Array(reader.result);
+        const oEXIF = getExif(buffer);
+        const canvas = document.getElementById("world");
+        const img = new Image();
+
+        img.onload = function () {
+            canvas.width = 512;
+            canvas.height = 512;
+            if (img.width > img.height) {
+                canvas.height = canvas.width * (img.height / img.width);
+            } else {
+                canvas.width = canvas.height * (img.width / img.height);
+            }
+            const ctx = canvas.getContext("2d");
+            ctx.putImageData(getCorrectOrientationImage(img,oEXIF.Orientation,canvas.width,canvas.height), 0,0);
+
+            const d64str = canvas.toDataURL("image/jpeg").replace(/^data:image\/jpeg;base64,/, "");
+            const imagePath = `${__dirname}/dropImage.jpg`.replace(/\\\\/g, "/");
+            fs.writeFileSync(imagePath, d64str, {
+                encoding: 'base64'
+            });
+
+            exec(`${pythonBin} ${tfHome}/tensorflow/models/image/imagenet/classify_image.py --image_file ${imagePath}`, (err, stdout, stderr) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log(stdout);
+
+                con.innerText = stdout;
+            });
+
+        }
+        img.src = URL.createObjectURL(new Blob([buffer], {
+            type: "image/jpeg"
+        }));
         con.innerText = "かんがえちゅう。。。";
     };
-
-    reader.readAsDataURL(file);
+    reader.readAsArrayBuffer(file);
     e.stopPropagation();
     con.innerText = "Drop end";
 }, false);
-
-/*
-const exec = require('child_process').exec;
-exec('c:/Users/kjw_j/anaconda3/python C:/Users/kjw_j/Anaconda3/Lib/site-packages/tensorflow/models/image/imagenet/classify_image.py', (err, stdout, stderr) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(stdout);
-});
-*/
